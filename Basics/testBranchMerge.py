545c61743a0d27c5e531becd5448b793e42e1d04
MADog23 <matt.dezemplen@gmail.com>
2024-07-25 18:05:09 -0500

    Added progress annotations in testBranchMerge.py

--- Basics/testBranchMerge.py
+++ Basics/testBranchMerge.py
@@ -1,3 +1,8 @@
 #The purpose of this file is to test branching and merging
 
-print("Branching is successful!")
+print("Branching is successful!") #Added in Matt Testing branch
+
+print("Branch Commit is successful!") #Added after committing with new branch
+
+print("Merging successful!") #Added after merging and deleting Matt Testing Branch
+ 
